f683fb76b9d6327952037d4fe7f71c87b1da8bcb
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-09-19 18:09:44 -0500

    Delete ish.py

--- ish.py
+++
@@ -1,3 +0,0 @@
-botoncon = tk.Button(banco, text="Retiro", font=("Bahnschrift SemiBold Condensed",25, BOLD), bg="yellow", fg="#271F26", width="40", height=2, bd=8, relief=RAISED,command=banco2)
-      botoncon.pack(pady=10)
-    
