5d556d02b695bff5f022137207901a8672092f84
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-09-19 18:09:18 -0500

    Create ish.py

---
+++ ish.py
@@ -0,0 +1,3 @@
+botoncon = tk.Button(banco, text="Retiro", font=("Bahnschrift SemiBold Condensed",25, BOLD), bg="yellow", fg="#271F26", width="40", height=2, bd=8, relief=RAISED,command=banco2)
+      botoncon.pack(pady=10)
+    
