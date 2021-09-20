5a179a359c24414d83fa5b96e28bb4140bc3cf95
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-09-19 21:44:18 -0500

    Update CAJERO Ean.py

--- CAJERO Ean.py
+++ CAJERO Ean.py
@@ -64,61 +64,46 @@ def login():
     loginto2.pack(pady=20)
     #
 
+    dinero=0
     def banco():
       entidades.withdraw()
       banco = tk.Toplevel()
       banco.geometry("600x500")
       banco.title("Banco EAN")
       banco.configure(bd=40, bg="#1586BF")
-      for i in usuarios:
-        for x in range(0, 4):
-          dinero = i[3]
-
+      
+      
       def saldo():
-        messagebox.showinfo("Saldo", "Tu saldo es de "+ str(dinero))
-
-      botoncon = tk.Button(banco, text="Consultar su saldo", cursor="hand2", font=("Bahnschrift SemiBold Condensed",25, BOLD), bg="purple", fg="#271F26", width="40", height=2, bd=8, relief=RAISED,command=saldo)
+        messagebox.showinfo("Saldo", "Tu saldo es de 10.000.000")
+      botoncon = tk.Button(banco, text="Consultar su saldo", font=("Bahnschrift SemiBold Condensed",25, BOLD), bg="purple", fg="#271F26", width="40", height=2, bd=8, relief=RAISED,command=saldo)
       botoncon.pack(pady=10)
       
 
-      Retirar = Label(banco, text="Retiro", font=("Bahnschrift SemiBold Condensed", 25, BOLD),
-                        bg="#60A420", fg="#271F26", width="40", height=2, bd=8, relief=RAISED)
-      Retirar.pack(pady=25)
-      messagebox.askokcancel("Retirar", "Retirando monto")
 
-      Transferir = Label(banco, text="Transferir dinero", font=("Bahnschrift SemiBold Condensed",25, BOLD), bg="orange", fg="#271F26", width="40", height=2, bd=8, relief=RAISED)
+      def xfa100profe():
+            banco.withdraw()
+            bancop = tk.Toplevel()
+            bancop.geometry("600x500")
+            bancop.title("Retiro EAN")
+            bancop.configure(bd=40, bg="#765048")
+            reti= Label(bancop, text="Digite el la cantidad que quiere retirar", font=("Bahnschrift SemiBold Condensed",25, BOLD), bg="purple", fg="#271F26", width="40", height=2, bd=8, relief=RAISED) 
+            reti.pack(pady=25)
+            clavecd= tk.Label(bancop, text="Monto:", bg="#60A420", fg="black")
+            clavecd.pack(pady=3, side=tk.TOP)
+            entrada200 = tk.Entry(bancop)
+            entrada200.pack(pady=7)
+            def profe100():
+                  messagebox.askokcancel("Retirar", "Retirando monto")
+                  messagebox.askokcancel("Retirar", "Su retiro a sido registrado con exito")
+            boton2222= Button(bancop, text="Siguiente ", font=("Bahnschrift SemiBold Condensed", 25, ITALIC, BOLD), command=profe100)
+            boton2222.pack(padx=20, pady=30)
+            
 
+          
+      botonpar100 = Button(banco, text='retiro', font=("ARIAL", 15), fg="black", command=xfa100profe)
+      botonpar100.pack(padx=20, pady=30)
       
-      
-      
-      botoncon = tk.Button(banco, text="Retiro", cursor="hand2", font=("Bahnschrift SemiBold Condensed",25, BOLD), bg="yellow", fg="#271F26", width="40", height=2, bd=8, relief=RAISED,command=banco2)
-      botoncon.pack(pady=10)
-      
-      
-      
-      
-      
-      
-      
-      
-      Transferir = Label(banco, text="Transferir dinero", font=("Bahnschrift SemiBold Condensed",
-                          25, BOLD), bg="orange", fg="#271F26", width="40", height=2, bd=8, relief=RAISED)
 
-      Transferir.pack(pady=25)
-
-      
-      
-      
-      
-      
-      
-      messagebox.askquestion("Transferir", "¿Esta seguro de tranferirlo?")
-      messagebox.askokcancel("Tranferiendo", "Enviendo el dinero")
-
-      
-      
-      
-      
     def Parqueadero():
       entidades.withdraw()
       Parqueadero = tk.Toplevel()
